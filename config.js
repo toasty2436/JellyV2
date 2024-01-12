28d7c4f151032dc0b6c449ecf4d0e3561dc57230
ToastedSploits <65846161+toasty2436@users.noreply.github.com>
2024-01-12 06:37:43 +0000

    Api

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ window.__CONFIG__ = {
   VITE_CORS_PROXY_URL: "https://jellyprox.ryanlepley84.workers.dev",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ODc3YmEzNjQ1YjM2MDJiZjYwMDBjMmM2MjA0ZWNkNyIsInN1YiI6IjY1MjdmYTgzODEzODMxMDEzOTBlN2UyMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.M_mU1r2y5qj8wSZnrH5NInccTUqEwsGKSjvn4DYPWHI",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjOWMwZWVhNzU4YTIxMTIyMGI1YTI5YTE0NTZhNTJjZSIsInN1YiI6IjY1MjdmYTgzODEzODMxMDEzOTBlN2UyMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lI9eaT6Iu6OySGlOB2eyGkNheSQvY-wBJzYtnNwtV_0",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: null,
